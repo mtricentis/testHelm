cdd9f0812f750557c0a2fc1295549c15053508c8
mtricentis <83463324+mtricentis@users.noreply.github.com>
2021-09-20 12:04:57 -0400

    Update repotag.sh

--- repotag.sh
+++ repotag.sh
@@ -31,7 +31,8 @@ git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.gi
 echo 'check current directory after Clone'
 pwd
 
-cp -r $FOLDER $CLONE_DIR
+#cp -r $FOLDER $CLONE_DIR
+cp -r . $CLONE_DIR
 cd $CLONE_DIR
 echo "After cd $CLONE_DIR"
 #git remote set-url origin2 https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git
